98c7315c52fab9a1a02b80faecd66f5771b675a4
WinsonWen <z5116007@zmail.unsw.edu.au>
2016-05-16 14:13:56 +1000

    Made a copy of mrPass called machanicalTurk so we don't have to rename when we submit

---
+++ mechanicalTurk.c
@@ -0,0 +1,28 @@
+/*
+ *  Mr Pass.  Brain the size of a planet!
+ *
+ *  Proundly Created by Richard Buckland
+ *  Share Freely Creative Commons SA-BY-NC 3.0. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "Game.h"
+#include "mechanicalTurk.h"
+
+action decideAction (Game g) {
+   action nextAction;
+   int currentPlayer = getWhoseTurn(g);
+   
+   if (currentPlayer != NO_ONE ) {
+      nextAction.actionCode = START_SPINOFF; //attempting to start a spinoff
+      if (isLegalAction(g, nextAction) == FALSE) {
+	      nextAction.actionCode = PASS; //If illegal move, then PASS
+      }
+   }
+   
+   return nextAction;
+}
